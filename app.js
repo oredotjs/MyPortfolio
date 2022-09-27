d107ec7094ef9a1e0a1b24167d09eb13dd6c880f
Emmanuel Oreoluwa <awuloero13@gmail.com>
2022-09-27 12:51:41 +0100

    :tada: initial

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const expressLayouts = require("express-ejs-layouts");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
@@ -6,10 +7,11 @@ dotenv.config({ path: "./config.env" });
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "10kb" }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(expressLayouts);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", function (req, res) {
   res.render("about");
